da86e5c24195959ef3a84f2fb5a77c990945e7a4
Joseph Smith <joe@layerful.com>
2018-05-13 18:20:24 +0100

    Added substruct

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import axios from 'axios'
+
+class Substruct {
+  constructor (token = false) {
+    if (!token) {
+      return new Error('You need to supply your space ID.')
+    } else {
+      this.secret = token
+    }
+    this.axios = axios.create({
+      baseURL: 'https://api.substruct.app/api/v1/space/'
+    })
+    this.axios.defaults.headers.common['Authorization'] = this.secret;
+  }
+
+  /**
+   * Function to collect pages
+   */
+  collectPages (callback, tags = []) {
+    this.axios({
+      method: 'get',
+      url: `pages${tags.length > 0 ? '?filter=' + tags.join(',') : ''}`
+    }).then((response) => {
+      if (response.status === 200) {
+        return callback(null, response.data)
+      } else {
+        return callback(response.data, null)
+      }
+    })
+  }
+
+  /**
+   * Function to collect page
+   */
+  collectPage (id, callback) {
+    this.axios({
+      method: 'get',
+      url: `page?id=${id}`
+    }).then((response) => {
+      if (response.status === 200) {
+        return callback(null, response.data)
+      } else {
+        return callback(response.data, null)
+      }
+    })
+  }
+}
+
+export default Substruct
